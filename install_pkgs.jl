0ff5abd1d5014574948280ef28d75ea5c3383a88
Aaron M. Watson <watsona4@gmail.com>
2019-08-28 13:56:17 +0000

    added installation script

---
+++ install_pkgs.jl
@@ -0,0 +1,42 @@
+start_pkg = ""
+
+skip = []
+
+rm("registries", recursive=true, force=true)
+
+using Pkg
+
+Pkg.add("ProgressMeter")
+Pkg.add(PackageSpec(url="https://github.com/wildart/TOML.jl.git"))
+
+home = pwd()
+
+using ProgressMeter
+using TOML
+
+general = TOML.parsefile("$home/registries/General/Registry.toml")
+
+packages = []
+for pkg in general["packages"]
+    push!(packages, pkg[2]["name"])
+end
+
+@showprogress for pkg_name in sort(packages)
+    if pkg_name > start_pkg && pkg_name ∉ skip
+        println("\nInstalling $pkg_name")
+        try
+            Pkg.add(pkg_name)
+            Pkg.build(pkg_name)
+        catch exc
+            println(exc)
+            if isa(exc, Pkg.Types.ResolverError)
+                continue
+            else
+                rethrow
+            end
+        end
+        run(`git add *`)
+        run(`git commit -m "added package $pkg_name"`)
+        run(`bash -c 'while :; do git push; if [[ $? == 0 ]]; then break; fi; done'`)
+    end
+end
