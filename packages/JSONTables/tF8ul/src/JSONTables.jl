fa97d7f3c824a359a6478a3e01bd97a86ca9ba8d
Aaron M. Watson <watsona4@gmail.com>
2019-09-02 00:56:47 +0000

    added package JSONTables

---
+++ packages/JSONTables/tF8ul/src/JSONTables.jl
@@ -0,0 +1,109 @@
+module JSONTables
+
+using JSON3, Tables
+
+export jsontable, arraytable, objecttable
+
+# read
+
+struct Table{columnar, T}
+    source::T
+end
+
+function jsontable(source)
+    x = JSON3.read(source)
+    columnar = x isa JSON3.Object
+    columnar || x isa JSON3.Array || throw(ArgumentError("input json source is not a table"))
+    return Table{columnar, typeof(x)}(x)
+end
+
+Tables.istable(::Type{<:Table}) = true
+
+# columnar source
+Tables.columnaccess(::Type{Table{true, T}}) where {T} = true
+Tables.columns(x::Table{true}) = x
+
+miss(x) = ifelse(x === nothing, missing, x)
+struct MissingVector{T} <: AbstractVector{T}
+    x::T
+end
+Base.IndexStyle(::Type{<:MissingVector}) = Base.IndexLinear()
+# Base.length(x::MissingVector) = length(x.x)
+Base.size(x::MissingVector) = size(x.x)
+@inline Base.getindex(x::MissingVector, i::Int) = miss(x.x[i])
+Base.copy(x::MissingVector) = map(y->y isa JSON3.Object || y isa JSON3.Array ? copy(y) : miss(y), x)
+
+Base.propertynames(x::Table{true}) = Tuple(keys(getfield(x, :source)))
+Base.getproperty(x::Table{true}, nm::Symbol) = MissingVector(getproperty(getfield(x, :source), nm))
+
+# row source
+Tables.rowaccess(::Type{Table{false, T}}) where {T} = true
+Tables.rows(x::Table{false}) = x
+
+Base.IteratorSize(::Type{Table{false, T}}) where {T} = Base.HasLength()
+Base.length(x::Table{false}) = length(x.source)
+Base.IteratorEltype(::Type{Table{false, T}}) where {T} = Base.HasEltype()
+Base.eltype(x::Table{false, JSON3.Array{T}}) where {T} = T
+
+struct MissingRow{T}
+    x::T
+end
+Base.propertynames(x::MissingRow) = propertynames(getfield(x, :x))
+Base.getproperty(x::MissingRow, nm::Symbol) = miss(getproperty(getfield(x, :x), nm))
+
+@inline function Base.iterate(x::Table{false})
+    st = iterate(x.source)
+    st === nothing && return nothing
+    val, state = st
+    return MissingRow(val), state
+end
+
+@inline function Base.iterate(x::Table{false}, st)
+    st = iterate(x.source, st)
+    st === nothing && return nothing
+    val, state = st
+    return MissingRow(val), state
+end
+
+# write
+struct ObjectTable{T}
+    x::T
+end
+
+JSON3.StructType(::Type{<:ObjectTable}) = JSON3.ObjectType()
+Base.pairs(x::ObjectTable) = zip(propertynames(x.x), Tables.eachcolumn(x.x))
+
+struct ArrayTable{T}
+    x::T
+end
+
+JSON3.StructType(::Type{<:ArrayTable}) = JSON3.ArrayType()
+
+struct ArrayRow{T}
+    x::T
+end
+
+JSON3.StructType(::Type{<:ArrayRow}) = JSON3.ObjectType()
+Base.pairs(x::ArrayRow) = zip(propertynames(x.x), Tables.eachcolumn(x.x))
+
+Base.IteratorSize(::Type{ArrayTable{T}}) where {T} = IteratorSize(T)
+Base.length(x::ArrayTable) = length(x.x)
+
+function Base.iterate(x::ArrayTable)
+    state = iterate(x.x)
+    state === nothing && return nothing
+    return ArrayRow(state[1]), state[2]
+end
+
+function Base.iterate(x::ArrayTable, st)
+    state = iterate(x.x, st)
+    state === nothing && return nothing
+    return ArrayRow(state[1]), state[2]
+end
+
+objecttable(table) = JSON3.write(ObjectTable(Tables.columns(table)))
+objecttable(io::IO, table) = JSON3.write(io, ObjectTable(Tables.columns(table)))
+arraytable(table) = JSON3.write(ArrayTable(Tables.rows(table)))
+arraytable(io::IO, table) = JSON3.write(io, ArrayTable(Tables.rows(table)))
+
+end # module
---
+++ packages/JSONTables/tF8ul/test/runtests.jl
@@ -0,0 +1,57 @@
+using Test, JSONTables, Tables, JSON3
+
+@testset "JSONTables" begin
+
+cjson = replace(replace("""{
+"a": [1,2,3],
+"b": [4.1, null, 6.3],
+"c": ["7", "8", null]
+}""", " "=>""), "\n"=>"")
+
+rjson = replace(replace("""[
+{"a": 1, "b": 4.1, "c": "7"},
+{"a": 2, "b": null, "c": "8"},
+{"a": 3, "b": 6.3, "c": null}
+]""", " "=>""), "\n"=>"")
+
+ctable = (a=[1,2,3], b=[4.1, missing, 6.3], c=["7", "8", missing])
+rtable = Tables.rowtable(ctable)
+
+cjtable = JSONTables.jsontable(cjson)
+rjtable = JSONTables.jsontable(rjson)
+
+@test Tables.istable(typeof(cjtable))
+@test Tables.istable(typeof(rjtable))
+
+@test Tables.columnaccess(typeof(cjtable))
+@test !Tables.rowaccess(typeof(cjtable))
+@test Tables.columns(cjtable) === cjtable
+@test propertynames(cjtable) == (:a, :b, :c)
+@test getproperty(cjtable, :a) == [1, 2, 3]
+
+@test !Tables.columnaccess(typeof(rjtable))
+@test Tables.rowaccess(typeof(rjtable))
+@test Tables.rows(rjtable) === rjtable
+@test Base.IteratorSize(typeof(rjtable)) == Base.HasLength()
+@test length(rjtable) == 3
+@test Base.IteratorEltype(typeof(rjtable)) == Base.HasEltype()
+@test eltype(rjtable) == Any
+
+@test isequal(Tables.columntable(cjtable), ctable)
+@test isequal(Tables.columntable(rjtable), ctable)
+@test isequal(Tables.rowtable(cjtable), rtable)
+@test isequal(Tables.rowtable(rjtable), rtable)
+
+@test JSONTables.objecttable(ctable) == cjson
+@test JSONTables.objecttable(rtable) == cjson
+@test JSONTables.arraytable(ctable) == rjson
+@test JSONTables.arraytable(rtable) == rjson
+
+io = IOBuffer()
+JSONTables.objecttable(io, ctable)
+@test String(take!(io)) == cjson
+
+JSONTables.arraytable(io, rtable)
+@test String(take!(io)) == rjson
+
+end
