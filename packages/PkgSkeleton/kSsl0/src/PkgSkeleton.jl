7105eb1b803f07928b9d5e99a48b9b67504629e8
Aaron M. Watson <watsona4@gmail.com>
2019-09-02 21:55:08 +0000

    added package PkgSkeleton

---
+++ packages/PkgSkeleton/kSsl0/src/PkgSkeleton.jl
@@ -0,0 +1,201 @@
+"""
+Julia package for creating new packages quickly. See [`PkgSkeleton.generate`](@ref).
+"""
+module PkgSkeleton
+
+using ArgCheck: @argcheck
+import Dates
+using DocStringExtensions: SIGNATURES
+import LibGit2
+import Pkg
+
+####
+####
+####
+
+####
+#### Template values
+####
+
+
+"""
+$(SIGNATURES)
+
+Populate a vector of replacement pairs, either from arguments or by querying global settings
+and state.
+"""
+function get_replacement_values(; pkg_name)
+    c = LibGit2.GitConfig()     # global configuration
+    _getgitopt(opt, type = AbstractString) = LibGit2.get(type, c, opt)
+    ["{UUID}" => Pkg.METADATA_compatible_uuid(pkg_name),
+     "{PKGNAME}" => pkg_name,
+     "{GHUSER}" => _getgitopt("github.user"),
+     "{USERNAME}" => _getgitopt("user.name"),
+     "{USEREMAIL}" => _getgitopt("user.email"),
+     "{YEAR}" => Dates.year(Dates.now())]
+end
+
+####
+#### Template application
+####
+
+"""
+$(SIGNATURES)
+
+Replace multiple pairs in `str`, using `replace` iteratively.
+"""
+replace_multiple(str, replacements) = foldl(replace, replacements; init = str)
+
+"""
+$(SIGNATURES)
+
+Copy from `src_dir` to `dest_dir` recursively, making the substitutions of
+
+1. file contents and
+
+2. filenames
+
+using `replacements`.
+
+Existing files are not overwritten when `skip_existing_files = true` (the default).
+
+Directory names are *not* replaced.
+
+Return a list of `source => dest` path pairs, with `source ≡ nothing` when `dest` was not
+overwritten.
+"""
+function copy_and_substitute(src_dir, dest_dir, replacements;
+                             skip_existing_files::Bool = true)
+    results = Vector{Pair{Union{String,Nothing},String}}()
+    for (root, dirs, files) in walkdir(src_dir)
+        sub_dir = relpath(root, src_dir)
+        mkpath(normpath(dest_dir, sub_dir))
+        for file in files
+            srcfile = joinpath(root, file)
+            destfile = normpath(joinpath(dest_dir, sub_dir,
+                                         replace_multiple(file, replacements)))
+            if isfile(destfile) && skip_existing_files
+                push!(results, nothing => destfile)
+            else
+                push!(results, srcfile => destfile)
+                srcstring = read(srcfile, String)
+                deststring = replace_multiple(srcstring, replacements)
+                write(destfile, deststring)
+            end
+        end
+    end
+    results
+end
+
+"""
+$(SIGNATURES)
+
+Return the template directory for `name`. Symbols refer to built-in templates, while strings
+are considered paths. Directories are always verified to exist.
+"""
+function resolve_template_dir(name::Symbol)
+    dir = abspath(joinpath(@__DIR__, "..", "templates", String(name)))
+    @argcheck isdir(dir) "Could not find built-in template $(name)."
+    dir
+end
+
+function resolve_template_dir(dir::AbstractString)
+    @argcheck isdir(dir) "Could not find directory $(dir)."
+    dir
+end
+
+"""
+$(SIGNATURES)
+
+Extract a package name using the last component of a path.
+
+The following all result in `"Foo"`:
+
+```julia
+pkg_name_from_path("/tmp/Foo")
+pkg_name_from_path("/tmp/Foo/")
+pkg_name_from_path("/tmp/Foo.jl")
+pkg_name_from_path("/tmp/Foo.jl/")
+```
+"""
+function pkg_name_from_path(path::AbstractString)
+    base, ext = splitext(basename(isdirpath(path) ? dirname(path) : path))
+    @argcheck isempty(ext) || ext == ".jl" "Invalid extension $(ext), specify package name manually."
+    base
+end
+
+####
+#### exposed API
+####
+
+"""
+$(SIGNATURES)
+
+Generate the skeleton for a Julia package in `dest_dir`.
+
+# Arguments
+
+`template` specifies the template to use. Symbols (eg `:default`, which is the default)
+refer to *built-in* templates delivered with this package. Strings are considered paths.
+
+`skip_existing_dir = true` (the default) aborts package generation for existing directories.
+
+`skip_existing_files = true` (the default) prevents existing files from being overwritten.
+
+`pkg_name` can be used to specify a package name. Note that it is derived from `dest_dir`:
+the package name is `"Foo"` for all of
+
+1. `"/tmp/Foo"`,
+2. `"/tmp/Foo/"`,
+3. `"/tmp/Foo.jl"`,
+4. `"/tmp/Foo.jl/"`,
+
+Use a different name only when you know what you are doing.
+
+`git_init = true` (the default) ensures that an *empty* repository is generated in
+`dest_dir`. You still have to commit files yourself.
+
+`docs_manifest` completes the `Manifest.toml` in the `docs` subdirectory. You usually want
+this.
+"""
+function generate(dest_dir; template = :default,
+                  skip_existing_dir::Bool = true,
+                  skip_existing_files::Bool = true,
+                  pkg_name = pkg_name_from_path(dest_dir),
+                  git_init::Bool = true, docs_manifest::Bool = true)
+    # preliminary checks
+    @argcheck !isfile(dest_dir) "destination $(dest_dir) is a file."
+    if skip_existing_dir && isdir(dest_dir)
+        @warn "destination $(dest_dir) exists, skipping package generation.\nConsider `skip_existing_dir = false`."
+        return false
+    end
+
+    # copy and substitute
+    @info "getting template values"
+    replacements = get_replacement_values(; pkg_name = pkg_name)
+    @info "copy and substitute"
+    results = copy_and_substitute(resolve_template_dir(template), dest_dir, replacements;
+                                  skip_existing_files = skip_existing_files)
+    for (src, dest) in results
+        src ≡ nothing && println(stderr, "not overwriting $(dest)")
+    end
+
+    # git initialization
+    if git_init
+        @info "initializing git repository"
+        LibGit2.init(dest_dir)
+    end
+
+    # docs manifest
+    if docs_manifest
+        @info "adding documenter (completing the Manifest.toml for docs)"
+        docs = joinpath(dest_dir, "docs")
+        run(`$(Base.julia_cmd()) --project=$(docs) -e 'import Pkg; Pkg.add("Documenter")'`)
+    end
+
+    # done
+    @info "successfully generated $(pkg_name)" dest_dir
+    true
+end
+
+end # module
---
+++ packages/PkgSkeleton/kSsl0/test/runtests.jl
@@ -0,0 +1,113 @@
+using PkgSkeleton, Test, Dates, UUIDs
+
+# import internals for testing
+using PkgSkeleton: get_replacement_values, resolve_template_dir, pkg_name_from_path,
+    replace_multiple
+
+####
+#### Command line git should be installed for tests (so that they don't depend in LibGit2).
+####
+
+if !success(`git --help`)
+    @info "Command line git should be installed."
+    exit(1)
+end
+
+####
+#### For CI, set up environment, otherwise use local settings (and assume they are defined).
+####
+
+const CI = parse(Bool, lowercase(get(ENV, "CI", "false")))
+
+function getgitopt(opt)
+    try
+        chomp(read(`git config --get $(opt)`, String))
+    catch
+        err("couldn't get git option $(opt)")
+    end
+end
+
+setgitopt(name, value) = run(`git config --global --add $(name) $(value)`)
+
+if CI
+    USERNAME = "Joe H. User"
+    USEREMAIL = "test@email.domain"
+    GHUSER = "somethingclever"
+    setgitopt("user.name", USERNAME)
+    setgitopt("user.email", USEREMAIL)
+    setgitopt("github.user", GHUSER)
+else
+    USERNAME = getgitopt("user.name")
+    USEREMAIL = getgitopt("user.email")
+    GHUSER = getgitopt("github.user")
+end
+
+####
+#### test components
+####
+
+@testset "replacement values" begin
+    d = Dict(get_replacement_values(; pkg_name = "FOO"))
+    @test d["{UUID}"] isa UUID
+    @test d["{GHUSER}"] == GHUSER
+    @test d["{USERNAME}"] == USERNAME
+    @test d["{USEREMAIL}"] == USEREMAIL
+    @test d["{YEAR}"] == year(now())
+end
+
+@testset "template directories" begin
+    default_template = abspath(joinpath(@__DIR__, "..", "templates", "default"))
+    @test resolve_template_dir(:default) == default_template
+    @test_throws ArgumentError resolve_template_dir(:nonexistent_builtin)
+    @test resolve_template_dir(default_template) == default_template
+    @test_throws ArgumentError resolve_template_dir(tempname()) # nonexistent
+end
+
+@testset "package name from path" begin
+    @test pkg_name_from_path("/tmp/FooBar") == "FooBar"
+    @test pkg_name_from_path("/tmp/FooBar.jl") == "FooBar"
+    @test pkg_name_from_path("/tmp/FooBar/") == "FooBar"
+    @test pkg_name_from_path("/tmp/FooBar.jl/") == "FooBar"
+    @test_throws ArgumentError pkg_name_from_path("/tmp/FooBar.bin/")
+end
+
+@testset "multiple replaces" begin
+    @test replace_multiple("{COLOR} {DISH}", ["{COLOR}" => "green", "{DISH}" => "curry"]) ==
+        "green curry"
+end
+
+####
+#### test generation
+####
+
+function check_dest_dir(pkg_name, dest_dir;)
+    # NOTE: very rudimentary check and should be improved, checking all valid substitutions
+    readme = joinpath(dest_dir, "README.md")
+    @test isfile(readme)
+    @test occursin(pkg_name, read(readme, String))
+    mainsrc = joinpath(dest_dir, "src", pkg_name * ".jl")
+    @test isfile(mainsrc)
+end
+
+@testset "package generation and checks" begin
+    mktempdir() do tempdir
+        dest_dir = joinpath(tempdir, "Foo")
+
+        # test generated structure
+        @test PkgSkeleton.generate(dest_dir) == true
+        check_dest_dir("Foo", dest_dir)
+
+        # run various sanity checks (mostly test contents of the template, CI will error)
+        cd(dest_dir) do
+            @info "test documentation (instantiation)"
+            run(`julia --project=docs -e 'using Pkg; Pkg.instantiate(); Pkg.develop(PackageSpec(path=pwd()))'`)
+            @info "test documentation (generation)"
+            run(`julia --project=docs --color=yes docs/make.jl`)
+            @info "test coverage (only instantiation)"
+            run(`julia --project=test/coverage -e 'using Pkg; Pkg.instantiate()'`)
+        end
+
+        @test PkgSkeleton.generate(dest_dir) == false # will not overwrite
+        @test PkgSkeleton.generate(dest_dir; skip_existing_dir = false) # will overwrite
+    end
+end
