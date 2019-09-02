5cf544d5fb5d9112b9c17b0077897c4ac30c1064
Aaron M. Watson <watsona4@gmail.com>
2019-09-02 00:38:37 +0000

    added package Impute

---
+++ packages/Impute/EgPBk/docs/make.jl
@@ -0,0 +1,24 @@
+using Documenter, Impute
+
+makedocs(
+    modules=[Impute],
+    format=:html,
+    pages=[
+        "Home" => "index.md",
+        "Impute" => "api/impute.md",
+        "Context" => "api/context.md",
+        "Imputors" => "api/imputors.md",
+        "Utilities" => "api/utils.md",
+    ],
+    repo="https://github.com/invenia/Impute.jl/blob/{commit}{path}#L{line}",
+    sitename="Impute.jl",
+    authors="Invenia Technical Computing Corporation",
+    assets=["assets/invenia.css"],
+)
+
+deploydocs(
+    repo = "github.com/invenia/Impute.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
+)
