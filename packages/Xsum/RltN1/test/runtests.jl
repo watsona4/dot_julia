21adf2708d755adb8d0d80fda2a4167fddf4665d
Aaron M. Watson <watsona4@gmail.com>
2019-09-04 10:38:17 +0000

    added package Xsum

---
+++ packages/Xsum/RltN1/test/runtests.jl
@@ -0,0 +1,21 @@
+using Xsum, Test, Random
+
+Random.seed!(314159)
+setprecision(65536)
+
+@testset "xsum" begin
+    for n in (100, 10^4)
+        a = randn(n)
+        exact = Float64.(sum(big.(a)))
+        @test exact == xsum(a)
+
+        A = [a a]
+        @test exact == xsum(@view A[:,1])
+        @test exact == xsum(@view Array(A')[1,:])
+
+        @test 2exact == xsum(2x for x in a)
+
+        @test Complex(2exact, 4exact) == xsum(Complex(2x,4x) for x in a)
+    end
+end
+
